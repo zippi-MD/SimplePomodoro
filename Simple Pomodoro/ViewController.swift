8cc991d5704743e6f8fa71589f467497a6d9b23a
Alejandro Mendoza <zippi180@gmail.com>
2020-06-14 15:45:53 -0500

    Initial Commit

---
+++ Simple Pomodoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Pomodoro
+//
+//  Created by Alejandro Mendoza on 14/06/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
